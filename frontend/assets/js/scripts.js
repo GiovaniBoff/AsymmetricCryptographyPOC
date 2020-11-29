f0275219a358f1058ea2e42478b0416187bcdfb1
Lucas <lucastovo@hotmail.com>
2020-11-29 15:06:38 -0300

    add modal with form and POST for create account at /users

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -1,14 +1,52 @@
 const base_url = "http://localhost:3333"
 
-const form = document.querySelector(".form-signin");
-const inputEmail = document.getElementById("inputEmail");
-const inputPassword = document.getElementById("inputPassword");
+const formLogin = document.getElementById("form-login");
+const formCreateAcc = document.getElementById("form-create-acc");
+const modalOpenBtn = document.getElementById("create-acc-btn");
+const modalCloseBtn = document.querySelector(".modal-wrapper>span");
+const modalCreateAcc = document.querySelector(".modal-create-acc");
 
-form.addEventListener('submit', async (e) => {
+formCreateAcc.addEventListener('submit', async (e) => {
     e.preventDefault();
-    let email = inputEmail.value;
-    let password = inputPassword.value;
-    let data = {
+    
+    const name = formCreateAcc.nome.value;
+    const email = formCreateAcc.email.value;
+    const password = formCreateAcc.password.value;
+    const data = {
+        name,
+        email,
+        password
+    }
+
+    try {
+        const req = await fetch(`${base_url}/users`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!req.ok) {
+            throw req;
+        }
+        
+        response = await req.json();
+        console.log(response);
+        const { token } = response;
+        sessionStorage.setItem('token', token);
+        console.log("########### DEU CERTO PORRA! ############")
+    } catch (error) {
+        console.error(error)
+    }
+
+})
+
+formLogin.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = formLogin.email.value;
+    const password = formLogin.password.value;
+    const data = {
         email,
         password
     }
@@ -35,3 +73,18 @@ form.addEventListener('submit', async (e) => {
         console.error(error)
     }
 })
+
+
+//HELPERS
+modalOpenBtn.onclick = toggleModal;
+modalCloseBtn.onclick = toggleModal;
+modalCreateAcc.onclick = (e) => {
+    if(e.target == modalCreateAcc){
+        toggleModal();
+    }
+}
+
+function toggleModal(){
+    modalCreateAcc.classList.toggle("toggle");
+}
+//#######
