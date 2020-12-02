2e5b78d0d08521977bed2a526c2c89d4d728f67a
Nikolas Freitas <nikolasbvf@gmail.com>
2020-12-02 01:25:36 -0300

    :sparkles: Add route protection

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,4 +1,5 @@
-import { encryptMessage } from './helpers/encrypt.js';
+import './services/protectPage.js';
+import { encryptMessage } from './services/encrypt.js';
 
 const button = document.querySelector('.get-things');
 
